bfbcaaa4382822e6dfac396f8adf998c8d65f679
andresife7 <andresi.fe@gmail.com>
2024-08-20 18:54:16 -0500

    feature/suma

--- operations.py
+++ operations.py
@@ -1,1 +1,8 @@
+# Función sum() en Python
 
+def sum():
+    numero1 = int(input("Coloca tu primer numero: "))
+    numero2 = int(input("Coloca tu segundo numero: "))
+    suma = numero1 + numero2    
+    print("El resultado de la suma es: " , suma)
+sum()
