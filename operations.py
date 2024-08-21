c6d502f825f472df601d639665b40c4ddc85615f
DiegoVG <davelasco2502@gmail.com>
2024-08-20 19:40:21 -0500

    merge div

--- operations.py
+++ operations.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 # Division with user input
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 result = a / b
 print(result)
-=======
 def multiplication():
     
     num1 = int(input("Enter the first number: "))
@@ -14,4 +12,3 @@ def multiplication():
 
 
 multiplication()
->>>>>>> 9b939a94b0870a4b0d40a06d97d1c91f045f7b1a
